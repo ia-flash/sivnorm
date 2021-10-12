24b2ad4c986fa29b1b6b61d7eb5ec852eecb08b6
Laurent Guichard <laurent1.guichard@interieur.gouv.fr>
2021-10-12 12:13:06 +0000

    Update setup.py --no-ci

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup, find_packages
 
 root_path = abspath(dirname(__file__))
 
-REQUIREMENTS = []
 with open(join(root_path, 'requirements.txt'), "rt") as f:
-    for line in f:
-        print("dd %s" % line)
+    REQUIREMENTS = [line.strip() for line in f]
+
 print("REQUIREMENTS %s  %s" % (join(root_path, 'requirements.txt'), REQUIREMENTS))
 setup(name="sivnorm", packages=find_packages(), install_requires=REQUIREMENTS)
