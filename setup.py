9ae697b3adcb4dfe9ee10b00de47a337ef572428
lguichard <laurent1.guichard@interieur.gouv.fr>
2022-08-10 16:49:19 +0200

    Ajout gestion de la version

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open(join(root_path, 'requirements.txt')) as f:
     REQUIREMENTS = [line.strip() for line in f]
 
 try:
-    TAG = environ['CI_COMMIT_TAG']
+    version = environ['CI_COMMIT_TAG']
 except KeyError:
-    TAG = input("Veuillez entrer la version de l'application:\n")
+    version = input("Entrez la version de l'application:\n")
 
 setup(name="sivnorm", 
     packages=find_packages(),
-    version=TAG,
+    version=version,
     install_requires=REQUIREMENTS,
     entry_points={'console_scripts': [
           'sivormapp = sivnorm.app:main'
